be4c06f1575637227924732e02104fe2bfa12117
gfellerleo <gfellerleo@gmail.com>
2024-06-03 15:30:44 +0200

    main

--- CCFraudAPP.py
+++ CCFraudAPP.py
@@ -11,16 +11,35 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
-import plotly.express as px
 
 
 data = pd.read_csv("creditcard_2023.csv")
 
 def home():
     st.title("Credit Card Fraud Detection")
-    st.subheader("Motivation")
-    st.write("Our team took on the task to develop a ML-modell which given certain parameters of a credit card transaction could determine wether a transaction was fraudulent or not. ")
+    st.subheader("Goal of the Project")
+    st.write("Our team took on the task to develop a ML-model which given certain parameters of a credit card transaction could determine wether a transaction was fraudulent or not. ")
 
+    st.subheader("Motivation")
+    st.markdown("""According to the 2023 credit card fraud report released by Security.org, that <span style="color: red;">65%</span> of <span style="color: red;">U.S adults</span> have atleast once expirienced a fraudulent transaction on their credit card. While <span style="color: red;">credit card fraud</span> numbers are <span style="color: red;">steady to rising in the U.S.</span>, the European Central Bank actually released statistics that show a <span style="color: red;">decline in creditcard fraud in Europe</span>. Eventhough those are good news for Europeans, the issue of credit card fraud is not going away any time soon. Credit card companies and criminals are in a rat race, where both become more sofisticated in either preventing or conducting credit card fraud. That is why <span style="color: red;">ML-Models</span> are becoming an immensly important tool to <span style="color: red;">recognize and flag fraudulent credit crad transactions</span> as reliably and quickly as possible. """, unsafe_allow_html=True)
+
+    st.title("Logistic Regression")
+    
+    st.subheader("What is Logistic Regression")
+    st.markdown("""Logistic Regression is a statistical method for binary classification. This means it helps us predict one of two possible outcomes. In our case that would be the distiction between fraudulant and non-fraudulant transaction. The model makes those decisions based on variouse factors and features.""", unsafe_allow_html=True)
+
+    st.subheader("How does it work?")
+    st.markdown("""<strong>Logistic Function:</strong> While linear regression models predict continuous values, a logistical regressionn model predicts probabilities. The outputs of the regression model are values between 0 and 1. This makes a logistical regression model perfect for binary classification tasks.""", unsafe_allow_html=True)
+    st.markdown("""<strong>Probability to Classes:</strong> After we get this probability as an output we set a threshold (usually 0.5) to classify the probablility into two classes. In our instance that would mean that if the probablity that a transaction is fraudulant is greater than 50 percent it would get flagged.""", unsafe_allow_html=True)
+    st.markdown("""<strong>Model training:</strong> During the models training it adjusts its parameters to best fit the data. This involves finding the best curve (example of curve below) that seperates the two classes by maximizing the likelihood of the observed data. This training takes place ina supervised enviroment.""", unsafe_allow_html=True)
+    with st.expander("Show logistical Regression Curve"):
+        st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Exam_pass_logistic_curve.svg/600px-Exam_pass_logistic_curve.svg.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+
+    st.subheader("Why are we using Logistic Regression")
+    st.markdown("""<strong>Simplicity:</strong> Logistic regression is simple and easy to implement. It's a good starting point for binary classification problems.""", unsafe_allow_html=True)
+    st.markdown("""<strong>Interpretability:</strong> The model is easy to interpret. The coefficients (parameters) can give insights into how each feature impacts the prediction.""", unsafe_allow_html=True)
+    st.markdown("""<strong>Efficiency:</strong> It is computationally efficient and works well with small to medium-sized datasets.""", unsafe_allow_html=True)
+    st.markdown("""<strong>Performance:</strong> Despite its simplicity, logistic regression often provides good performance and can be a strong baseline for more complex models.""", unsafe_allow_html=True)
 def data_exploration():
     st.title("Data Exploration")
     st.subheader("Raw Data")
@@ -35,14 +54,12 @@ def data_exploration():
         st.write(data.shape)
         st.write(data.head())
 
-    if st.checkbox('Show interactive correlation heatmap'):
-        fig = px.imshow(data.corr(), text_auto=True, aspect='auto', color_continuous_scale='viridis')
-        st.plotly_chart(fig)
-    else:  # Add this else statement to keep the original behavior
-        heatmap = plt.figure(figsize=[20,10])
-        sns.heatmap(data.corr(), cmap="crest", annot=True)
-        with st.expander("Show correlation heatmap"):
-            st.pyplot(heatmap)
+    st.subheader("Correlation Matrix")
+    st.markdown("""The highest correlation between parameters are between <span style="color: red;">V16</span>, <span style="color: red;">V17</span> and <span style="color: red;">V18</span>.""", unsafe_allow_html=True)
+    heatmap = plt.figure(figsize=[20,10])
+    sns.heatmap(data.corr(),cmap="crest", annot=True)
+    with st.expander("Show correlation heatmap"):
+        st.pyplot(heatmap)
 
 
     st.subheader("Feature Correlations with Class")
@@ -81,7 +98,7 @@ def data_exploration():
     with st.expander("Show Visual for Amount or Class"):
         visual = st.selectbox("Select Visual", ["Distribution of Amount", "Distribution of Class"])
         
-        if visual == "Distribution of Amount":
+        if visual == "KDE Plot of Amount":
             plt.figure(figsize=[10, 6])
             sns.kdeplot(data=data["Amount"], fill=True, color="skyblue", bw_adjust=0.5)
             plt.title("KDE Plot of Transaction Amount")
@@ -90,19 +107,26 @@ def data_exploration():
             plt.grid(True)
             st.pyplot(plt)
 
-        elif visual == "Distribution of Class":
-            class_counts = data["Class"].value_counts()
-            labels = ["Not Fraudulent", "Fraudulent"]
-            colors = ["lightblue", "red"]
-    
-            fig = px.pie(data, names=class_counts.index, title="Distribution of Class", 
-                 values=class_counts.values, color_discrete_sequence=colors, 
-                 hole=0.3, labels=labels)
-    
-            st.plotly_chart(fig)
+        elif visual == "Class Distribution Pie Chart":
+            fig, ax = plt.subplots(figsize=[10, 6], facecolor='#2e2e2e')  # Anthracite background
+            fig.patch.set_facecolor('#2e2e2e')  # Set the figure background color
+
+            data["Class"].value_counts().plot.pie(
+                autopct="%3.1f%%", 
+                colors=["lightblue", "red"], 
+                startangle=140, 
+                explode=(0.1, 0), 
+                ax=ax
+            )
+            plt.title("Distribution of Class", color='white')
+            plt.ylabel("")  # Hide the y-label for better visual appeal
+            ax.legend(["Not Fraudulent", "Fraudulent"], loc="upper right", framealpha=0.5)
+            plt.setp(ax.get_legend().get_texts(), color='white')  # Set legend text color to white
+            plt.setp(ax.texts, color='white')  # Set pie chart text color to white
+            st.pyplot(fig)
 
 
-def model_training():   
+def model_training():
 
     x = data.drop(['id','Class'],axis=1) # Id not important for our use
     y = data.Class # Save Class attribute (fraudelent/non-fraudulent in own variable)
@@ -153,7 +177,7 @@ def model_training():
         preds_lr_train = lr.predict(x_train)
         preds_lr_test = lr.predict(x_test)
         
-        st.write("Modell Accuracy")
+        st.write("Model Accuracy")
         model_eval(y_train, preds_lr_train)
 
         st.write("Test Accuracy")
@@ -164,13 +188,10 @@ def model_training():
 def model_eval(actual, predicted):
     acc_score = accuracy_score(actual, predicted)
     conf_matrix = confusion_matrix(actual, predicted)
-    class_rep = classification_report(actual, predicted)
-
-    st.write(f"Accuracy Score: {round(acc_score, 2)}")
-    st.write("Confusion Matrix:")
-    st.write(conf_matrix)
-    st.write("Classification Report:")
-    st.write(class_rep)
+    class_rep = classification_report (actual, predicted)
+    print("Model Accuracy: ", round(acc_score, 2))
+    print(conf_matrix)
+    print(class_rep)
 
 
 # Create a sidebar with navigation options
