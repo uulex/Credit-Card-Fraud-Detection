b4ef453db6cbeafd10beea140b0c3613ac92e256
gfellerleo <gfellerleo@gmail.com>
2024-06-21 14:35:13 +0200

    Proofread and fixed about 20 grammatical errors

--- CCFraudAPP.py
+++ CCFraudAPP.py
@@ -19,20 +19,20 @@ data = pd.read_csv("creditcard_2023.csv")
 def home():
     st.title("Credit Card Fraud Detection")
     st.subheader("Goal of the Project")
-    st.write("Our team took on the task to develop a ML-model which given certain parameters of a credit card transaction could determine wether a transaction was fraudulent or not. ")
+    st.write("Our team took on the task to develop a ML-model which given certain parameters of a credit card transaction could determine whether a transaction was fraudulent or not. ")
 
     st.subheader("Motivation")
-    st.markdown("""According to the 2023 credit card fraud report released by Security.org, that <span style="color: red;">65%</span> of <span style="color: red;">U.S adults</span> have atleast once expirienced a fraudulent transaction on their credit card. While <span style="color: red;">credit card fraud</span> numbers are <span style="color: red;">steady to rising in the U.S.</span>, the European Central Bank actually released statistics that show a <span style="color: red;">decline in creditcard fraud in Europe</span>. Eventhough those are good news for Europeans, the issue of credit card fraud is not going away any time soon. Credit card companies and criminals are in a rat race, where both become more sofisticated in either preventing or conducting credit card fraud. That is why <span style="color: red;">ML-Models</span> are becoming an immensly important tool to <span style="color: red;">recognize and flag fraudulent credit crad transactions</span> as reliably and quickly as possible. """, unsafe_allow_html=True)
+    st.markdown("""According to the 2023 credit card fraud report released by Security.org, that <span style="color: red;">65%</span> of <span style="color: red;">U.S adults</span> have atleast once experienced a fraudulent transaction on their credit card. While <span style="color: red;">credit card fraud</span> numbers are <span style="color: red;">steady to rising in the U.S.</span>, the European Central Bank  released statistics that show a <span style="color: red;">decline in creditcard fraud in Europe</span>. Even though those are good news for Europeans, the issue of credit card fraud is not going away any time soon. Credit card companies and criminals are in a rat race, where both become more sophisticated in either preventing or conducting credit card fraud. That is why <span style="color: red;">ML-Models</span> are becoming an immensly important tool to <span style="color: red;">recognize and flag fraudulent credit crad transactions</span> as reliably and quickly as possible. """, unsafe_allow_html=True)
 
     st.title("Logistic Regression")
     
     st.subheader("What is Logistic Regression")
-    st.markdown("""Logistic Regression is a statistical method for binary classification. This means it helps us predict one of two possible outcomes. In our case that would be the distiction between fraudulant and non-fraudulant transaction. The model makes those decisions based on variouse factors and features.""", unsafe_allow_html=True)
+    st.markdown("""Logistic Regression is a statistical method for binary classification. This means it helps us predict one of two possible outcomes. In our case that would be the distinction between fraudulent and non-fraudulent transactions. The model makes those decisions based on various factors and features.""", unsafe_allow_html=True)
 
     st.subheader("How does it work?")
-    st.markdown("""<strong>Logistic Function:</strong> While linear regression models predict continuous values, a logistical regressionn model predicts probabilities. The outputs of the regression model are values between 0 and 1. This makes a logistical regression model perfect for binary classification tasks.""", unsafe_allow_html=True)
-    st.markdown("""<strong>Probability to Classes:</strong> After we get this probability as an output we set a threshold (usually 0.5) to classify the probablility into two classes. In our instance that would mean that if the probablity that a transaction is fraudulant is greater than 50 percent it would get flagged.""", unsafe_allow_html=True)
-    st.markdown("""<strong>Model training:</strong> During the models training it adjusts its parameters to best fit the data. This involves finding the best curve (example of curve below) that seperates the two classes by maximizing the likelihood of the observed data. This training takes place ina supervised enviroment.""", unsafe_allow_html=True)
+    st.markdown("""<strong>Logistic Function:</strong> While linear regression models predict continuous values, a logistical regression model predicts probabilities. The outputs of the regression model are values between 0 and 1. This makes a logistical regression model perfect for binary classification tasks.""", unsafe_allow_html=True)
+    st.markdown("""<strong>Probability to Classes:</strong> After we get this probability as an output we set a threshold (usually 0.5) to classify the probablility into two classes. In our instance that would mean that if the probablity that a transaction is fraudulent is greater than 50 percent, it would get flagged.""", unsafe_allow_html=True)
+    st.markdown("""<strong>Model training:</strong> During the models training, it adjusts its parameters to best fit the data. This involves finding the best curve (example of curve below) that seperates the two classes by maximizing the likelihood of the observed data. This training takes place in a supervised environment.""", unsafe_allow_html=True)
     with st.expander("Show logistical Regression Curve"):
         st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Exam_pass_logistic_curve.svg/600px-Exam_pass_logistic_curve.svg.png", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
@@ -45,7 +45,7 @@ def data_exploration():
     st.title("Data Exploration")
     st.subheader("Raw Data")
     st.markdown("""
-    The columns of our dataset consist of <span style="color: red;">28 anonymized Parameters</span>, 
+    The columns of our dataset consists of <span style="color: red;">28 anonymized Parameters</span>, 
     the <span style="color: red;">transaction-amount in $</span>, a  
     <span style="color: red;">unique Id</span>, and the <span style="color: red;">Class</span> which determines 
     if a transaction was <span style="color: red;">fraudulent or not</span> (1 = fraudulent; 0 = not fraudulent). 
@@ -136,10 +136,10 @@ def model_training():
     model to <span style="color: red;">predict</span>, that is our <span style="color: red;">dependent</span> feature. 
     <span style="color: red;">All other features</span> are <span style="color: red;">independent</span>. 
     Apart from our feature <span style="color: red;">Class</span>, we will also <span style="color: red;">drop</span> 
-    the feature <span style="color: red;">Id</span> since it just is <span style="color: red;">not important</span> 
+    the feature <span style="color: red;">Id</span> since it is <span style="color: red;">not important</span> 
     for the prediction. In our <span style="color: red;">preprocessing</span> step, we use 
     <span style="color: red;">standardization</span> to bring all our <span style="color: red;">features</span> 
-    on the <span style="color: red;">same scale</span>.
+    to the <span style="color: red;">same scale</span>.
     """, unsafe_allow_html=True)
 
     st.subheader("Building our ML-Model")
@@ -147,7 +147,7 @@ def model_training():
     We saw our best chances at building a highly accurate ML-Model in the <span style="color: red;">logistic regression</span> 
     approach. We decided to go with this approach because it is a <span style="color: red;">supervised machine learning algorithm</span>, 
     which is used for <span style="color: red;">binary classification tasks</span> (in our case fraudulent/non-fraudulent) 
-    and is <span style="color: red;">perfect for predictive modeling</span>. While we already did our data preparation and preprocessing 
+    and is <span style="color: red;">perfect for predictive modeling</span>. While we already did our data preparation and preprocessing,
     we still need to <span style="color: red;">divide</span> our dataset into a <span style="color: red;">training-set</span> 
     and a <span style="color: red;">test-set</span>. For this, we went with a <span style="color: red;">3/4</span> to 
     <span style="color: red;">1/4</span> distribution.
@@ -170,7 +170,7 @@ def model_training():
         train_class_report = classification_report(y_train, y_train_pred, output_dict=True)
         test_class_report = classification_report(y_test, y_test_pred, output_dict=True)
         st.balloons()
-        st.markdown("<h2 style='text-align: center;'>Model ran succefully!</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Model ran successfully!</h2>", unsafe_allow_html=True)
         st.markdown("<h3>Training Accuracy</h3>", unsafe_allow_html=True)
         st.write(f"<div style='text-align: center; font-size: 24px; color: green;'>{train_accuracy:.2f}</div>", unsafe_allow_html=True)
         
@@ -212,7 +212,7 @@ def model_training():
         test_class_report = classification_report(y_test, y_test_pred, output_dict=True)
 
         st.balloons()
-        st.markdown("<h2 style='text-align: center;'>Model ran succefully!</h2>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center;'>Model ran successfully!</h2>", unsafe_allow_html=True)
 
         # Display results
         st.markdown("<h3>Training Accuracy</h3>", unsafe_allow_html=True)
